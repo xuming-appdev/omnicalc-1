b8b5a9471584d8a7b2a111072d2895b7bf7c3bda
xumingmonica101 <ming.xu@chicagobooth.edu>
2022-04-21 05:46:33 +0000

    sqaure root donw

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,18 @@ def calculate_square
   render({ :template=> "calculation_templates/square_results.html.erb"})
 end
 
+def blank_square_root_form
+
+  render({ :template=> "calculation_templates/square_root_form.html.erb"})
+end
+
+def calculate_square_root
+
+  @num=params.fetch("elephant").to_f
+  @square_root_of_num=@num **0.5
+
+  render({ :template=> "calculation_templates/square_root_results.html.erb"})
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ get("/square/new", { :controller =>"application", :action =>"blank_square_form"}
 
 get("/square/results", { :controller =>"application", :action =>"calculate_square"})
 
+get("/square_root/new", { :controller =>"application", :action =>"blank_square_root_form"})
+
+get("/square_root/results", { :controller =>"application", :action =>"calculate_square_root"})
+
 end
